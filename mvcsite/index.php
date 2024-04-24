5113b0625cfb7449a126140fcc7c3072022c74b7
EvgeNik321 <167080406+EvgeNik321@users.noreply.github.com>
2024-04-24 12:44:05 +0300

    Update index.php

--- mvcsite/index.php
+++ mvcsite/index.php
@@ -1,13 +1,13 @@
 <?
 
 require_once ("database.php");
-require_once ("models/articles.php");
+require_once ("models/article.php");
 
 
 $link = db_connect();
 $articles = articles_all($link);
 
 
-include("views/articles.php");
+include("views/article.php");
 
 ?>
