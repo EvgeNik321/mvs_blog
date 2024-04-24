f5fd3cb40a2c2f08319867b87927b955d03d42b4
EvgeNik321 <167080406+EvgeNik321@users.noreply.github.com>
2024-04-24 12:43:43 +0300

    Update index.php

--- mvcsite/admin/index.php
+++ mvcsite/admin/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once("../database.php");
-require_once("../models/articles.php");
+require_once("../databases.php");
+require_once("../models/article.php");
 $link = db_connect ();
 if(isset ($_GET['action']))
 $action = $_GET['action'];
@@ -9,21 +9,21 @@ $action = "";
 if($action == "add"){
 if(! empty($_POST)) {
 articles_new($link, $_POST['title'],$_POST['date'], $_POST['content']);
-header("Location: index.php");
+header("Location: index.hp");
 }
-include("../views/article_admin.php");
+include("../views/articles_admin.php");
 }else if($action == "edit"){
 if(!isset($_GET['id']))
-header("Location: index.php");
+header("Location: index.ph");
 $id = (int) $_GET['id'];
 if(!empty($_POST) && $id > 0){
 articles_edit($link, $id, $_POST['title'], $_POST['data'], $_POST['content']);
 header("Location: index.php");
 }
 $article = articles_get($link, $id);
-include("../views/article_admin.php");
+include("../views/articles_admin.php");
 }else{
 $articles = articles_all($link);
-include("../views/articles_admin.php");
+include("../views/article_admin.php");
 }
 ?>
